8fd9df9dca1a8ba20f30873f37114fe58f3ce20f
Smithadodmane <smithadodmane@gmail.com>
2024-10-10 11:34:58 +0530

    added code

---
+++ bandnamegenerator.py
@@ -0,0 +1,11 @@
+"""Create a greeting for your program.
+Ask the user for the city that they grew up in and store it in a variable.
+Ask the user for the name of a pet and store it in a variable.
+Combine the name of their city and pet and show them their band name"""
+
+print("Welcome to the BAND NAME GENERATOR")
+city=input("what is the city name you grew up?")
+print(city)
+pet=input("what is your pet name?")
+print(pet)
+print("your band name can be " +city +" "+pet)
